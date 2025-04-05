b799db5f3b9a9d6ec34d9d3d91093ba2705a5899
ismaelhugo <ismael.oliveira@somagrupo.com.br>
2025-04-05 18:19:30 -0300

    clean project

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl font-bold text-center">
+        Operação olhos de aguia
+      </h1>
+      <h2 className="text-2xl font-bold text-center">
+        Em breve...
+      </h2>
+    </div>
+  );
+}
